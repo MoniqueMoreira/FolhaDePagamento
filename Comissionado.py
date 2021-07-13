4fa1e7938c0376367a906e968cc284be7328ea9f
Monique Moreira <mmm@ic.ufal.br>
2021-07-13 14:21:09 -0300

    Correção Comissionado

--- Comissionado.py
+++ Comissionado.py
@@ -18,20 +18,20 @@ class Comissionado(Empregados):
         while(i!=1):
             k=int(input("Deseja ALTERA qual dados do empregado:\n1-Nome\n2-Endereço\n3-Forma de Pagamento\n4-Sindicato\n5-Valor Salário\n>>>"))
             if k == 1:
-                super().setNome(self)
+                super().setNome()
                 i=1
             elif k==2:
-                super().setEndereco(self)
+                super().setEndereco()
                 i=1
             elif k==3:
-                super().setPagamento(self)
+                super().setPagamento()
                 i=1
             elif k==4:
-                super().setSindicato(self)
+                super().setSindicato()
                 i=1
             elif k==5:
-                Comissionado.setSalario(self)
-                Comissionado.setPercentual(self)
+                Comissionado.setSalario()
+                Comissionado.setPercentual()
                 i=1
             else:
                 print("Opção Inválida")
